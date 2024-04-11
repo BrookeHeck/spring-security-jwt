64a038dcf008d87e6425774037a393ae32f6315f
BrookeHeck <b19heck@gmail.com>
2024-04-10 21:39:39 -0400

    implement method to generate jwt

--- flashcard.wars/src/main/java/study/games/flashcard/wars/auth/JwtService.java
+++ flashcard.wars/src/main/java/study/games/flashcard/wars/auth/JwtService.java
@@ -1,8 +1,32 @@
 package study.games.flashcard.wars.auth;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import org.springframework.beans.factory.annotation.Value;
+
+import java.util.Date;
+
+import static study.games.flashcard.wars.auth.SecurityConstants.*;
 
 public class JwtService {
     @Value("${spring.security.secret-key}")
     private String secret;
+
+    public String generateJwt(UserPrinciple userPrinciple) {
+        String[] claims = getClaimsFromUserPrinciple(userPrinciple);
+        Date now = new Date();
+        return JWT.create()
+                .withIssuer(JWT_ISSUER)
+                .withAudience(JWT_AUDIENCE)
+                .withIssuedAt(now)
+                .withSubject(userPrinciple.getUsername())
+                .withArrayClaim(AUTHORITIES, claims)
+                .withExpiresAt(new Date(now.getTime() + EXPIRATION_TIME))
+                .sign(Algorithm.HMAC512(secret.getBytes()));
+    }
+
+    private String[] getClaimsFromUserPrinciple(UserPrinciple userPrinciple) {
+
+    }
+
 }
