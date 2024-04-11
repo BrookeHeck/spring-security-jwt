31beafe270541f2e8979e70980b30fa5b61ba572
BrookeHeck <b19heck@gmail.com>
2024-04-10 21:44:40 -0400

    implement method to get claims from user principle

--- flashcard.wars/src/main/java/study/games/flashcard/wars/auth/JwtService.java
+++ flashcard.wars/src/main/java/study/games/flashcard/wars/auth/JwtService.java
@@ -3,8 +3,11 @@ package study.games.flashcard.wars.auth;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.GrantedAuthority;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import static study.games.flashcard.wars.auth.SecurityConstants.*;
 
@@ -13,20 +16,24 @@ public class JwtService {
     private String secret;
 
     public String generateJwt(UserPrinciple userPrinciple) {
-        String[] claims = getClaimsFromUserPrinciple(userPrinciple);
+        List<String> claims = getClaimsFromUserPrinciple(userPrinciple);
         Date now = new Date();
         return JWT.create()
                 .withIssuer(JWT_ISSUER)
                 .withAudience(JWT_AUDIENCE)
                 .withIssuedAt(now)
                 .withSubject(userPrinciple.getUsername())
-                .withArrayClaim(AUTHORITIES, claims)
+                .withArrayClaim(AUTHORITIES, claims.toArray(new String[0]))
                 .withExpiresAt(new Date(now.getTime() + EXPIRATION_TIME))
                 .sign(Algorithm.HMAC512(secret.getBytes()));
     }
 
-    private String[] getClaimsFromUserPrinciple(UserPrinciple userPrinciple) {
-
+    private List<String> getClaimsFromUserPrinciple(UserPrinciple userPrinciple) {
+        List<String> authorities = new ArrayList<>();
+        for(GrantedAuthority grantedAuthority : userPrinciple.getAuthorities()) {
+            authorities.add(grantedAuthority.getAuthority());
+        }
+        return authorities;
     }
 
 }
--- flashcard.wars/src/main/java/study/games/flashcard/wars/auth/SecurityConstants.java
+++ flashcard.wars/src/main/java/study/games/flashcard/wars/auth/SecurityConstants.java
@@ -1,7 +1,7 @@
 package study.games.flashcard.wars.auth;
 
 public class SecurityConstants {
-    public static final long EXPIRATION_TIME = 3_600_000;
+    public static final long EXPIRATION_TIME = 3_600_000; // 1 hour
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String JWT_HEADER = "jwt-header";
     public static final String AUTHORITIES = "Authorities";
