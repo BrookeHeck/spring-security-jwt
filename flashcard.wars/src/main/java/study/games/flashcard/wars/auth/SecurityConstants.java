551ea5a440639c1838e86a2edd5372bfd2e8d1cb
BrookeHeck <b19heck@gmail.com>
2024-04-10 21:06:45 -0400

    add security constants

---
+++ flashcard.wars/src/main/java/study/games/flashcard/wars/auth/SecurityConstants.java
@@ -0,0 +1,10 @@
+package study.games.flashcard.wars.auth;
+
+public class SecurityConstants {
+    public static final long EXPIRATION_TIME = 3_600_000;
+    public static final String TOKEN_HEADER = "Bearer ";
+    public static final String JWT_HEADER = "jwt-header";
+    public static final String JWT_CANNOT_BE_VERIFIED = "JWT cannot be verified";
+    public static final String AUTHORITIES = "Authorities";
+    
+}
