8c73292b85575a798c92e92d635ed10f772df392
BrookeHeck <b19heck@gmail.com>
2024-04-28 17:43:16 -0400

    add authentication provider beans to configuration file and finsih security filter chain

--- flashcard.wars/src/main/java/study/games/flashcard/wars/auth/SecurityConfiguration.java
+++ flashcard.wars/src/main/java/study/games/flashcard/wars/auth/SecurityConfiguration.java
@@ -1,10 +1,13 @@
 package study.games.flashcard.wars.auth;
 
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
@@ -14,19 +17,29 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import java.util.List;
 
 @Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfiguration {
+    private final AuthenticationProvider authenticationProvider;
+
     @Bean
     protected SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
+                .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(auth -> auth
-                                .requestMatchers("/**")
+                                .requestMatchers(SecurityConstants.PUBLIC_URLS[0])
                                 .permitAll()
-//                        .anyRequest()
-//                        .authenticated()
+                                .requestMatchers(SecurityConstants.PUBLIC_URLS[1])
+                                .permitAll()
+                                .requestMatchers(SecurityConstants.PUBLIC_URLS[2])
+                                .permitAll()
+                                .anyRequest()
+                                .authenticated()
                 )
                 .sessionManagement(sessionManagementConfigurer -> {
                     sessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
                 })
+                .authenticationProvider(authenticationProvider)
                 .httpBasic(Customizer.withDefaults());
         return httpSecurity.build();
     }
