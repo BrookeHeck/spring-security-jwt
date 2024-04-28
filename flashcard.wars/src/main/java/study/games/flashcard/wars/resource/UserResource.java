828f70461852d38c92f012c1ff4d8a351e7e91e0
BrookeHeck <b19heck@gmail.com>
2024-04-28 18:24:13 -0400

    add controlelr test methods to make sure security config is working

--- flashcard.wars/src/main/java/study/games/flashcard/wars/resource/UserResource.java
+++ flashcard.wars/src/main/java/study/games/flashcard/wars/resource/UserResource.java
@@ -1,10 +1,32 @@
 package study.games.flashcard.wars.resource;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/user")
 public class UserResource {
+    @GetMapping(value = "login")
+    public String login() {
+        return "LOGIN";
+    }
+
+    @GetMapping(value = "register")
+    public String register() {
+        return "REGISTER";
+    }
+
+    @GetMapping(value = "reset-password/{email}")
+    public String resetPassword(@PathVariable String email) {
+        return email;
+    }
+
+    @GetMapping(value = "home")
+    public String home() {
+        return "HOME";
+    }
+
 
 }
