cdb09f96d9c9da49eb1a5978dc668c9b4e2a2cfb
BrookeHeck <b19heck@gmail.com>
2024-04-28 19:14:40 -0400

    add access denied handler

--- flashcard.wars/src/main/java/study/games/flashcard/wars/exception/domain/ExceptionHandling.java
+++ flashcard.wars/src/main/java/study/games/flashcard/wars/exception/domain/ExceptionHandling.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import study.games.flashcard.wars.models.dtos.Response;
 
+import java.nio.file.AccessDeniedException;
 import java.time.LocalDateTime;
 
 @ControllerAdvice
@@ -21,14 +22,20 @@ public class ExceptionHandling extends ResponseEntityExceptionHandler {
     @ExceptionHandler(DisabledException.class)
     public ResponseEntity<Response<String>> accountDisabledException() {
         logger.error("Attempted login of disabled user");
-        return createHttpResponse(HttpStatus.FORBIDDEN,
-                "Your account is disabled. If this is a mistake please create a help ticket");
+        return createHttpResponse(HttpStatus.UNAUTHORIZED,
+                "Your account is disabled. If this is a mistake please create a help ticket.");
     }
 
     @ExceptionHandler(BadCredentialsException.class)
     public ResponseEntity<Response<String>> badCredentialsException() {
         logger.error("Attempted login with bad credentials");
-        return createHttpResponse(HttpStatus.FORBIDDEN, "Incorrect username and/or password");
+        return createHttpResponse(HttpStatus.FORBIDDEN, "Incorrect username and/or password.");
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<Response<String>> accessDeniedException() {
+        logger.error("User attempted to access unauthorized content");
+        return createHttpResponse(HttpStatus.FORBIDDEN, "You do not have access to this content");
     }
 
     
