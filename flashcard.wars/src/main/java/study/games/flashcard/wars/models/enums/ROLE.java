d8c7e66b0ec2661f1185f4c394abe53d35a2ddcd
BrookeHeck <b19heck@gmail.com>
2024-05-05 10:33:58 -0400

    add the list of permissions to the role enum for each role

--- flashcard.wars/src/main/java/study/games/flashcard/wars/models/enums/ROLE.java
+++ flashcard.wars/src/main/java/study/games/flashcard/wars/models/enums/ROLE.java
@@ -2,10 +2,22 @@ package study.games.flashcard.wars.models.enums;
 
 import study.games.flashcard.wars.RolePermissions;
 
+import java.util.List;
+
+import static study.games.flashcard.wars.RolePermissions.*;
+
 public enum ROLE {
-    STUDENT(RolePermissions.STUDENT_PERMISSIONS),
-    TEACHER,
-    ADMIN,
-    TEACHER_ASSISTANT
+    STUDENT(STUDENT_PERMISSION),
+    TEACHER(TEACHER_PERMISSIONS),
+    ADMIN(ADMIN_PERMISSIONS),
+    TEACHER_ASSISTANT(TEACHER_ASSISTANT_PERMISSIONS);
+
+    private final List<PERMISSION> permissions;
+
+    ROLE(List<PERMISSION> permissions) {
+        this.permissions = permissions;
+    }
+
+    public List<PERMISSION> getPermissions() {return permissions;}
 
 }
