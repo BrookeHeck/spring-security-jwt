fd7b48b7c0e27b01c3b4d752fce53d99070a02a3
BrookeHeck <b19heck@gmail.com>
2024-05-11 20:23:11 -0400

    call auth service to authenticate user and create jwt and return both from controller

--- flashcard.wars/src/main/java/study/games/flashcard/wars/resource/UserResource.java
+++ flashcard.wars/src/main/java/study/games/flashcard/wars/resource/UserResource.java
@@ -2,15 +2,18 @@ package study.games.flashcard.wars.resource;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import study.games.flashcard.wars.auth.AuthenticationService;
 import study.games.flashcard.wars.models.dtos.Response;
 import study.games.flashcard.wars.models.dtos.UserDto;
 import study.games.flashcard.wars.models.entities.AppUser;
-import study.games.flashcard.wars.service.UserService;
 
 import java.time.LocalDateTime;
+
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static org.springframework.http.HttpStatus.OK;
+import static study.games.flashcard.wars.auth.SecurityConstants.JWT_HEADER;
 
 @RequiredArgsConstructor
 @RestController
@@ -19,10 +22,12 @@ public class UserResource {
     private final AuthenticationService authService;
 
     @GetMapping(value = "login")
-    public Response<AppUser> login(@RequestHeader(HttpHeaders.AUTHORIZATION) String basicAuthHeader) {
+    public ResponseEntity<AppUser> login(@RequestHeader(AUTHORIZATION) String basicAuthHeader) {
         AppUser user = authService.login(basicAuthHeader);
-
-        return null;
+        String jwt = authService.generateJwtToken(user);
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add(JWT_HEADER, jwt);
+        return new ResponseEntity<>(user, httpHeaders, OK);
     }
 
     @PostMapping(value = "register")
@@ -31,8 +36,8 @@ public class UserResource {
 //        registeredDto.setPassword(null);
         return Response.<AppUser>builder()
                 .timeStamp(LocalDateTime.now())
-                .httpStatusCode(HttpStatus.OK.value())
-                .httpStatus(HttpStatus.OK)
+                .httpStatusCode(OK.value())
+                .httpStatus(OK)
                 .data(registeredDto)
                 .message("registration successful")
                 .build();
