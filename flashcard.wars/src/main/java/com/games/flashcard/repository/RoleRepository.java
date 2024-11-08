9ce38fea55658d3ea088ce868a32c15532d520bd
BrookeHeck <b19heck@gmail.com>
2024-11-08 11:49:05 -0500

    remove unused imports

--- flashcard.wars/src/main/java/com/games/flashcard/repository/RoleRepository.java
+++ flashcard.wars/src/main/java/com/games/flashcard/repository/RoleRepository.java
@@ -2,8 +2,6 @@ package com.games.flashcard.repository;
 
 import com.games.flashcard.model.entities.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
     Role findRoleById(long id);
