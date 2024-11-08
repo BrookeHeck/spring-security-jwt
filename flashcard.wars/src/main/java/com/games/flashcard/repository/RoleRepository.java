86b7c7b9be0c0030ba6f0c309c3ee11d232a0186
BrookeHeck <b19heck@gmail.com>
2024-11-08 11:51:07 -0500

    add two more querires to resource for managing roles

--- flashcard.wars/src/main/java/com/games/flashcard/repository/RoleRepository.java
+++ flashcard.wars/src/main/java/com/games/flashcard/repository/RoleRepository.java
@@ -1,6 +1,7 @@
 package com.games.flashcard.repository;
 
 import com.games.flashcard.model.entities.Role;
+import com.games.flashcard.model.enums.ROLE;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
@@ -11,4 +12,8 @@ public interface RoleRepository extends JpaRepository<Role, Long> {
     Role findRoleByUserId(long userId);
 
     Role findRoleByOrganizationId(long organizationId);
+
+    Role findRoleByRole(ROLE role);
+
+    Role findRoleByRoleAndOrganizationId(ROLE role, long organizationId);
 }
