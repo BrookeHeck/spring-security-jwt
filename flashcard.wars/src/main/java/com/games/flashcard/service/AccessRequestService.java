18c7676c94f3756e134528e5bca48c4a3d3fa678
BrookeHeck <b19heck@gmail.com>
2024-11-19 08:18:41 -0500

    implement access request service

--- flashcard.wars/src/main/java/com/games/flashcard/service/AccessRequestService.java
+++ flashcard.wars/src/main/java/com/games/flashcard/service/AccessRequestService.java
@@ -1,15 +1,14 @@
 package com.games.flashcard.service;
 
 import com.games.flashcard.model.dtos.NewOrganizationRequestDto;
-import com.games.flashcard.model.entities.StudentAccessRequest;
+import com.games.flashcard.model.dtos.StudentAccessRequestDto;
 
 import java.util.List;
 
 public interface AccessRequestService {
     List<NewOrganizationRequestDto> getAllNewOrganizationRequests();
     void deleteOldOrganizationRequests();
-    void deleteNewOrganizationRequestById(long orgId);
-    List<StudentAccessRequest> getStudentAccessRequestsByOrganizationId();
-    void deleteOldStudentAccessRequests();
+    void deleteNewOrganizationRequestById(long requestId);
+    List<StudentAccessRequestDto> getStudentAccessRequestsByOrganizationId(long orgId);
     void deleteStudentAccessRequestById(long requestId);
 }
