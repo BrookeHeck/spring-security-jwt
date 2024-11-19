d211b651603fd5dcdf5dea77a5d3d92fe4c2d21e
BrookeHeck <b19heck@gmail.com>
2024-11-19 08:12:49 -0500

    add queries to student acceess request repo

--- flashcard.wars/src/main/java/com/games/flashcard/repository/StudentAccessRequestRepository.java
+++ flashcard.wars/src/main/java/com/games/flashcard/repository/StudentAccessRequestRepository.java
@@ -3,5 +3,10 @@ package com.games.flashcard.repository;
 import com.games.flashcard.model.entities.StudentAccessRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface StudentAccessRequestRepository extends JpaRepository<StudentAccessRequest, Long> {
+    List<StudentAccessRequest> findStudentAccessRequestByOrganizationId(long orgId);
+
+    void deleteStudentAccessRequestById(long requestId);
 }
