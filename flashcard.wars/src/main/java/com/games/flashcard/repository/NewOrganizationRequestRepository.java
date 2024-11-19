f917c327f62ad3744a5f44fe9723f3306c0a8105
BrookeHeck <b19heck@gmail.com>
2024-11-19 08:09:19 -0500

    add queries to new organization request repo and create service for managing access requests for organizations and students

--- flashcard.wars/src/main/java/com/games/flashcard/repository/NewOrganizationRequestRepository.java
+++ flashcard.wars/src/main/java/com/games/flashcard/repository/NewOrganizationRequestRepository.java
@@ -1,7 +1,22 @@
 package com.games.flashcard.repository;
 
 import com.games.flashcard.model.entities.NewOrganizationRequest;
+import com.games.flashcard.model.entities.StudentAccessRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 public interface NewOrganizationRequestRepository extends JpaRepository<NewOrganizationRequest, Long> {
+    @Query("select request from NewOrganizationRequest request")
+    List<NewOrganizationRequest> getAllOrganizationRequests();
+
+    @Modifying
+    @Query("delete NewOrganizationRequest request where request.timeOfInsert > :THREE_MONTHS_AGO")
+    void deleteOldOrganizationRequests(LocalDateTime threeMonthsAgo);
+
+    void deleteNewOrganizationRequestById(long orgId);
+
 }
---
+++ flashcard.wars/src/main/java/com/games/flashcard/service/AccessRequestService.java
@@ -0,0 +1,16 @@
+package com.games.flashcard.service;
+
+import com.games.flashcard.model.dtos.NewOrganizationRequestDto;
+import com.games.flashcard.model.dtos.OrganizationDto;
+import com.games.flashcard.model.entities.StudentAccessRequest;
+
+import java.util.List;
+
+public interface AccessRequestService {
+    List<NewOrganizationRequestDto> getAllNewOrganizationRequests();
+    void deleteOldOrganizationRequests();
+    void deleteNewOrganizationRequestById(long orgId);
+    List<StudentAccessRequest> getStudentAccessRequestsByOrganizationId();
+    void deleteOldStudentAccessRequests();
+    void deleteStudentAccessRequestById(long requestId);
+}
